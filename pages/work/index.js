110866481912bf1b00d5901ce5c5b2c1b78af2f3
Noé Zaabi <noe@Noes-MacBook-Pro.local>
2023-07-23 22:50:57 +0200

    Project and contact form

--- pages/work/index.js
+++ pages/work/index.js
@@ -1,5 +1,49 @@
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+import WorkSlider from "../../components/WorkSlider";
+import Circles from "../../components/Circles";
 const Work = () => {
-  return <div>Work</div>;
+  return (
+    <div className="h-full bg-primary/30 py-36 flex items-center">
+      <Circles />
+      <div className="container mx-auto pt-36 md:pt-40">
+        <div className="flex flex-col xl:flex-row gap-x-8">
+          {/* text */}
+          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4">
+            <motion.h2
+              variants={fadeIn("up", 0.2)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="h2 xl:mt-12"
+            >
+              My Work <span className="text-accent">.</span>
+            </motion.h2>
+            <motion.p
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="mb-4 max-w-[400px] mx-auto lg:mx-0"
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+              voluptatum, quibusdam, quia, voluptate quod quos dolorum.
+            </motion.p>
+          </div>
+          {/* slider */}
+          <motion.div
+            variants={fadeIn("down", 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="w-full xl:max-w-[65%]"
+          >
+            <WorkSlider />
+          </motion.div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Work;
