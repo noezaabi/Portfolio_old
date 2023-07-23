fd40ba5cda38b9137bfe184fa10231bcc43033a1
Noé Zaabi <noe@Noes-MacBook-Pro.local>
2023-07-23 20:19:12 +0200

    Main page + nav + animations

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,24 @@
-const Layout = () => {
-  return <div>Layout</div>;
+import { Sora } from "@next/font/google";
+import Nav from "./Nav";
+import Header from "./Header";
+import TopLeftImg from "./TopLeftImg";
+const sora = Sora({
+  subsets: ["latin"],
+  variables: "--font-sora",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+});
+
+const Layout = ({ children }) => {
+  return (
+    <div
+      className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variables} font-sora relative`}
+    >
+      <TopLeftImg />
+      <Nav />
+      <Header />
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,21 @@
+import Link from "next/link";
+
+import { RiTwitterLine, RiLinkedinLine, RiGithubLine } from "react-icons/ri";
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link href={"https://twitter.com/ZaabiNoe"} className="">
+        <RiTwitterLine className="hover:text-accent transition-all duration-300" />
+      </Link>
+      <Link href={"https://github.com/noezaabi"} className="">
+        <RiGithubLine className="hover:text-accent transition-all duration-300" />
+      </Link>
+      <Link href={"https://www.linkedin.com/in/noe-zaabi/"} className="">
+        <RiLinkedinLine className="hover:text-accent transition-all duration-300" />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
