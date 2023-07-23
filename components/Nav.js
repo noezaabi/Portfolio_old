b05cea86268ca4e3e52b2d0124bc1d3694269896
Noé Zaabi <noe@Noes-MacBook-Pro.local>
2023-07-23 20:49:33 +0200

    mobile size fix

--- components/Nav.js
+++ components/Nav.js
@@ -31,9 +31,9 @@ const Nav = () => {
   const router = useRouter();
   const pathname = router.pathname;
   return (
-    <nav className="flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen">
+    <nav className="flex flex-col items-center xl:justify-center gap-y-4 fixed h-14 md:h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen">
       {/* Inner */}
-      <div className="flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[80px] bg-white/10 xl:h-max py-8 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full">
+      <div className="flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-8 md:px-40 xl:px-0 h-[80px] bg-white/10 xl:h-max py-2 md:py-8 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full">
         {links.map((link, index) => (
           <Link
             className={`${
